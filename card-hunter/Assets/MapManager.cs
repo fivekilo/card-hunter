a9d8bfba932b3b610b82ed6bfbe496714a80e922
fivekilo <3060882350@qq.com>
2025-05-12 21:24:14 +0800

    移动命令改为协程模式

--- card-hunter/Assets/MapManager.cs
+++ card-hunter/Assets/MapManager.cs
@@ -182,7 +182,7 @@ public class MapManager : MonoBehaviour
         }
         return true;
     }
-    public Vector2Int MoveCommand(List<Vector2Int> directions, Vector2Int player,Vector2Int length)//移动指令
+    public IEnumerable MoveCommand(List<Vector2Int> directions, Vector2Int player,Vector2Int length)//移动指令
     {
         //还没添加越过障碍物功能
         List<Vector2Int> ObPosition = map.GetObstacles();
@@ -218,8 +218,7 @@ public class MapManager : MonoBehaviour
         }
 
         ClickedPos = new Vector2Int(-1, -1);
-        while (!accessible.Contains(ClickedPos)) ;
-        return ClickedPos;
+        yield return new WaitUntil(()=>accessible.Contains(ClickedPos));
     }
     public Vector3 GetVector3(Vector2Int pos)
     {
