a1c6f41c42a01d8ff4519746428da0e7a12260d1
rainstar0320 <2056960956@qq.com>
2025-05-14 22:54:30 +0800

    完善怪物移动机制，在Battlemanage中添加怪物进行回合

--- card-hunter/Assets/BattleManager.cs
+++ card-hunter/Assets/BattleManager.cs
@@ -168,6 +168,17 @@ public class BattleManager : MonoBehaviour
         }
     }
 
+    private IEnumerator HandEnemyTurn()
+    {
+        foreach(var enemy in _enemies)
+        {
+            if (enemy != null)
+            {
+                yield return enemy.TakeTurn();
+            }
+        }
+        ChangeState(BattleState.PlayerDraw);
+    }
 
     private IEnumerator PlayerDrawPhase()
     {
