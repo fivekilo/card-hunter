f2ba1a6da47c46c4c9178092dd76e200ad216a52
SunlightC1215 <843525811@qq.com>
2025-05-15 11:37:22 +0800

    修改手牌渲染顺序问题

--- card-hunter/Assets/Card.cs
+++ card-hunter/Assets/Card.cs
@@ -10,16 +10,12 @@ using UnityEngine.UI;
 public class Card : MonoBehaviour
 {
 
-    //private Vector2 hoverScale = new Vector2(1.2f, 1.2f);
-    //private Vector2 normalScale = Vector2.one;
+    //public string cardId;//创建的卡牌对象名称
     public Canvas parentCanvas;
-    //public int cardRarity { get; set; }
     public string cardText;
     public string cardName;
     public string cardType;
 
-    //private bool isDragging = false;
-    //private Vector2 originalPosition;
     public int cardNum { get; set; }
     public List<int> Move; //移动方向 正前方0 逆时针标号 不移动为null
     public Vector2Int MoveLength;//移动长度 下上限
@@ -113,65 +109,7 @@ public class Card : MonoBehaviour
         CBuse = true;
     }
     
-    //public void OnPointerClick(PointerEventData eventData)
-    //{
-    //    Debug.Log("Card clicked: " + cardNum);
-    //}
-    //public void OnBeginDrag(PointerEventData eventData)
-    //{
-    //    isDragging = true;
-    //    // 提高卡牌层级，确保它在其他卡牌上方
-    //    GetComponentInChildren<Canvas>().sortingOrder += 10;
-    //}
-    //public void OnDrag(PointerEventData eventData)
-    //{
-    //    // 将屏幕坐标转换为世界坐标
-    //    Vector2 mousePos = Camera.main.ScreenToWorldPoint(eventData.position);
-    //    Debug.Log(mousePos.x);
-    //    Debug.Log(mousePos.y);
-    //    //mousePos.z = 0; // 确保z坐标为0
-    //    transform.position = mousePos;
-    //}
-
-    //public void OnEndDrag(PointerEventData eventData)
-    //{
-    //    isDragging = false;
-    //    GetComponentInChildren<Canvas>().sortingOrder -= 10;
-
-    //    // 检查是否放置在有效区域
-    //    if (!IsValidDropZone())
-    //    {
-    //        Debug.Log("被移回");
-    //        // 如果不是有效区域，返回原位
-    //        transform.position = originalPosition;
-    //    }
-    //}
-
-    //private bool IsValidDropZone()
-    //{
-    //    // 这里实现检查是否在有效放置区域的逻辑
-    //    // 可以使用射线检测或其他方法
-    //    return true; // 暂时返回true
-    //}
-    //public void OnPointerEnter(PointerEventData eventData)
-    //{
-    //    if (!isDragging)
-    //    {
-    //        //transform.localScale = hoverScale;
-    //        // 提高层级确保悬停卡牌在最上方
-    //        GetComponentInChildren<Canvas>().sortingOrder += 5;
-    //    }
-    //}
-    //public void OnPointerExit(PointerEventData eventData)
-    //{
-    //    if (!isDragging)
-    //    {
-    //        //transform.localScale = normalScale;
-    //        // 恢复层级
-    //        GetComponentInChildren<Canvas>().sortingOrder -= 5;
-    //    }
-    //}
-        //Start is called before the first frame update
+ 
         void Start()
         {
 
