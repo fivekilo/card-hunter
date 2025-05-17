e873b5db69418fafadc744a841972251f4cd0e11
fivekilo <3060882350@qq.com>
2025-05-17 21:09:59 +0800

    创建场景间共享数据类

--- card-hunter/Assets/OuterScripts/GameManager.cs
+++ card-hunter/Assets/OuterScripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public SharedData shareddata;
     public GameObject camp;
     public GameObject WorldMap;
     public GameObject Player;
---
+++ card-hunter/Assets/Resources/SharedData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedData : ScriptableObject
+{
+    public PlayerInfo playerinfo;
+    public Commission commission;
+}
