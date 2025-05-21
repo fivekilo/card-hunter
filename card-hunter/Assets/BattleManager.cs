169718911c3267165b6dcb4ce4f6be00a0d4cf44
rainstar0320 <2056960956@qq.com>
2025-05-22 01:49:53 +0800

    怪物预制体初始化修复

--- card-hunter/Assets/BattleManager.cs
+++ card-hunter/Assets/BattleManager.cs
@@ -77,6 +77,68 @@ public class BattleManager : MonoBehaviour
     public bool isWaitingForPlayerChoose = false;
     public UnityEvent EndTurnClicked;
 
+    //怪物初始化相关
+    public SharedData shareddata;
+    public string choosetype;
+    public MonsterPrefabType monster;
+    public enum MonsterPrefabType
+    {
+        Dazeilong,
+        Xuanniao,
+        Manelong,
+        Yanzeilong,
+        Leilanglong,
+        Bingzhoulong
+    }
+    // 在 Inspector 中配置预制体引用
+    [System.Serializable]
+    public class PrefabMapping
+    {
+        public MonsterPrefabType type;
+        public GameObject prefab;
+    }
+    [SerializeField] private PrefabMapping[] prefabMappings; // 预制体映射配置
+    //加载怪物预制体
+    public void InitializePrefab(
+        MonsterPrefabType type,
+        Vector3 position,
+        Quaternion rotation,
+        Transform parent)
+    {
+        // 查找对应类型的预制体
+        foreach (var mapping in prefabMappings)
+        {
+            if (mapping.type == type)
+            {
+                // 实例化预制体
+                GameObject instance = Instantiate(
+                    mapping.prefab,
+                    position,
+                    rotation,
+                    parent);
+
+            }
+        }
+    }
+    public void ChooseMonsterType()
+    {
+        choosetype = shareddata.commission.monster;
+        switch (choosetype)
+        {
+            case ("大贼龙"):
+                monster = MonsterPrefabType.Dazeilong; break;
+            case ("眩鸟"):
+                monster = MonsterPrefabType.Xuanniao; break;
+            case ("蛮颚龙"):
+                monster = MonsterPrefabType.Manelong; break;
+            case ("岩贼龙"):
+                monster = MonsterPrefabType.Yanzeilong; break;
+            case ("雷狼龙"):
+                monster = MonsterPrefabType.Leilanglong; break;
+            case ("冰咒龙"):
+                monster = MonsterPrefabType.Bingzhoulong; break;
+        }
+    }
 
     private void UpdatedeckNum()
     {
@@ -154,7 +216,11 @@ public class BattleManager : MonoBehaviour
         Endbutton.onClick.AddListener(OnEndTurnButtonClicked);
         Movebutton.onClick.AddListener(OnMoveButtonClicked);
 
- 
+        ChooseMonsterType();
+        InitializePrefab(monster,
+        Vector3.zero,
+        Quaternion.identity,
+        transform);
         /*  Endbutton.onClick.AddListener(() => {
              Card newcard= cardManager.CreateCard(i, cardManager.transform);
               cardManager.AddCardToHand(newcard , hand);
